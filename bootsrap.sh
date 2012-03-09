20fd2e94835356f5b292d706da70a25a26e726ae
Emile Swarts <emile.swarts123@gmail.com>
2012-03-09 17:54:33 +0000

    Remove echos.  Live now

--- bootsrap.sh
+++ bootsrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "rm -fr ~/dotfiles"
+rm -fr ~/dotfiles
 
-echo "git clone git@github.com:emileswarts/dotfiles.git dotfiles"
+git clone git@github.com:emileswarts/dotfiles.git dotfiles
 
 for file in ` ls -a ~/dotfiles | grep -v '^\.\.$' |grep -v '^\.$' | grep -v '^README$' |grep -v '^compiz-settings.profile$' |grep -v '^gnome-terminal-conf.xml$' |grep -v '^bootstrap.sh$'`
 do
-    echo "rm -fr ~/$file"
-	echo "ln -s ~/dotfiles/$file ~/$file"
+    rm ~/$file
+	ln -s ~/dotfiles/$file ~/$file
 done
