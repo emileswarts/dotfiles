43c7b0ea74bec8dc57f9a7daf35e886ec1cb0d65
Emile Swarts <emile.swarts123@gmail.com>
2012-03-10 09:02:09 +0000

    use user variable

--- bootsrap.sh
+++ bootsrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-rm -fr ~/dotfiles
-
-git clone git@github.com:emileswarts/dotfiles.git dotfiles
-
-chown -R emile dotfiles
+cd
+rm -fr $HOME/dotfiles
+git clone git@github.com:emileswarts/dotfiles.git "$HOME/dotfiles"
+chown -R $USER dotfiles
 
 for file in ` ls -a ~/dotfiles | grep -v '^\.\.$' |grep -v '^\.$' | grep -v '^README$' |grep -v '^compiz-settings.profile$' |grep -v '^gnome-terminal-conf.xml$' |grep -v '^bootstrap.sh$'`
 do
-    rm -f ~/$file
-	ln -s ~/dotfiles/$file ~/$file
+    rm -f $HOME/$file
+	ln -s "$HOME/dotfiles/$file" "$HOME/$file"
 done
